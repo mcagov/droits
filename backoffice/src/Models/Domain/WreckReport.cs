7d79a0c706b7ab4d2bc960fb811697c390a468f8
Sam Kendell <sam.kendell@madetech.com>
2023-02-15 16:53:49 +0000

    backoffice init

---
+++ backoffice/src/Models/Domain/WreckReport.cs
@@ -0,0 +1,7 @@
+namespace src.Models.Domain
+{
+    public class WreckReport
+    {
+
+    }
+}
---
+++ backoffice/src/Program.cs
@@ -0,0 +1,34 @@
+using Droits.Models;
+using GovUk.Frontend.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<DroitsContext>(opt => opt.UseInMemoryDatabase("droits"));
+builder.Services.AddGovUkFrontend();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
