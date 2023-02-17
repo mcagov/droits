7c7082b5285b57c9d11b08b57e25d9b0ae025079
Evie Skinner <evie.skinner@madetech.com>
2023-02-17 16:32:11 +0000

    Add /home/droits test endpoint

--- backoffice/src/Controllers/HomeController.cs
+++ backoffice/src/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Droits()
+    {
+        return new OkResult();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
