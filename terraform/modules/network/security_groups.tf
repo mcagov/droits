8aa20363c030cb10fb7c17c2a41a4e66a524140d
Sam <sam.brown@madetech.com>
2023-02-24 11:39:56 +0000

    move network outputs to own output file

--- terraform/modules/network/security_groups.tf
+++ terraform/modules/network/security_groups.tf
@@ -2,27 +2,3 @@ resource "aws_security_group" "api-backoffice" {
   name = "api-backoffice"
   vpc_id = var.aws_vpc_id
 }
-
-output "api-backoffice-id" {
-  value = aws_security_group.api-backoffice.id
-}
-
-output "vpc-id" {
-  value = var.aws_vpc_id
-}
-
-output "private-subnet-1" {
-  value = var.private_subnet_1
-}
-
-output "private-subnet-2" {
-  value = var.private_subnet_2
-}
-
-output "public-subnet-1" {
-  value = var.public_subnet_1
-}
-
-output "public-subnet-2" {
-  value = var.public_subnet_2
-}
