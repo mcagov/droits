c295c7b72c6d74c27d4a9a4dbc232a7b52945185
Sam <sam.brown@madetech.com>
2023-03-14 09:51:27 +0000

    add security group for backoffice load balancer

--- terraform/modules/network/security_groups.tf
+++ terraform/modules/network/security_groups.tf
@@ -2,3 +2,7 @@ resource "aws_security_group" "api-backoffice" {
   name = "api-backoffice"
   vpc_id = var.aws_vpc_id
 }
+resource "aws_security_group" "api-backoffice-lb" {
+  name = "api-backoffice-lb"
+  vpc_id = var.aws_vpc_id
+}
