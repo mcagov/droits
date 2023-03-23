03336d41961336bf3f437100c83fe4d38f2cd67a
Evie Skinner <evie.skinner@madetech.com>
2023-03-23 14:59:59 +0000

    Restrict inbound traffic to DB: only the backoffice ECS service is allowed in

--- terraform/modules/security-groups/security_groups.tf
+++ terraform/modules/security-groups/security_groups.tf
@@ -44,6 +44,21 @@ resource "aws_security_group" "api-backoffice-lb" {
 resource "aws_security_group" "droits-db" {
   name = "droits-db"
   vpc_id = var.aws_vpc_id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 5432
+    to_port         = 5432
+    security_groups = [ aws_security_group.api-backoffice.id ]
+    description = "Allow inbound access from the api-backoffice ECS service only"
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outbound access to anywhere"
+  }
 }
 resource "aws_security_group" "webapp" {
   name = "webapp"
