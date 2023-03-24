9b5e5d1b8626df5cd378c460eab3d6c7ba909c9e
Sam <sam.brown@madetech.com>
2023-03-24 14:33:48 +0000

    Reopen backoffice service

--- terraform/modules/security-groups/security_groups.tf
+++ terraform/modules/security-groups/security_groups.tf
@@ -2,11 +2,12 @@ resource "aws_security_group" "api-backoffice" {
   name   = "api-backoffice"
   vpc_id = var.aws_vpc_id
   ingress {
-    from_port       = 0
-    to_port         = 65535
-    protocol        = "tcp"
-    security_groups = [aws_security_group.api-backoffice-lb.id]
-    description     = "Allow inbound access from the backoffice LB only"
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    #    security_groups = [aws_security_group.api-backoffice-lb.id]
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow inbound access from the backoffice LB only"
   }
   egress {
     protocol    = "-1"
