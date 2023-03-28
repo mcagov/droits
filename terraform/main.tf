f6ac50c7ad9354803d10ee4a2aa68076036480ea
Sam Kendell <sam.kendell@madetech.com>
2023-03-28 15:10:50 +0100

    added db instance type var

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,6 +42,7 @@ module "rds" {
   source               = "./modules/rds"
   db_password          = var.db_password
   db_username          = var.db_username
+  db_instance_class    = var.db_instance_class
 }
 
 resource "aws_s3_bucket" "droits-wreck-images" {
