38aac0009446644cfc0c2962ee8a59f19074c7fd
Sam Kendell <sam.kendell@madetech.com>
2023-04-24 17:12:45 +0100

    merged in main

--- webapp/services/azure-upload.js
+++ webapp/services/azure-upload.js
@@ -1,8 +1,8 @@
 import { BlobServiceClient } from '@azure/storage-blob';
 import fs from 'fs';
 const mime = require('mime-types');
-const dotenv = require('dotenv');
-dotenv.config();
+require("dotenv-json")();
+
 export const azureUpload = async (image, imageName) => {
   // Create the BlobServiceClient object which will be used to create a container client
   const blobServiceClient = BlobServiceClient.fromConnectionString(
