63b86d9acbc9c86c4f7aed8f851284e43b0a104d
Evie Skinner <evie.skinner@madetech.com>
2023-06-01 14:25:45 +0100

    Add inset text formatting. Remove empty lines from preview. Add values to other personalised params

--- backoffice/src/Models/FormView/EmailForm.cs
+++ backoffice/src/Models/FormView/EmailForm.cs
@@ -15,15 +15,15 @@ namespace Droits.Models
             => new (){
                     { "reference", "123"},
                     { "custom message", Subject},
-                    { "hazardous find", "no"},
-                    { "mmo", "no"},
-                    { "wreck add info", "no"},
-                    { "item add info", "no"},
-                    {"find add info", "no"},
-                    { "rip", "no"},
-                    {"rip no recover", "no"},
-                    { "archaeological protocol", "no"},
-                    { "more info", "no"}
+                    { "hazardous find", "yes"},
+                    { "mmo", "yes"},
+                    { "wreck add info", "yes"},
+                    { "item add info", "yes"},
+                    {"find add info", "yes"},
+                    { "rip", "yes"},
+                    {"rip no recover", "yes"},
+                    { "archaeological protocol", "yes"},
+                    { "more info", "yes"}
             };
     }
 }
