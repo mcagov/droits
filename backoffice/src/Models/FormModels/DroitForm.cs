77a0bd4376fc0af0e5b111550829447a3066a834
Sam Kendell <sam.kendell@madetech.com>
2023-06-02 12:20:21 +0100

    formatting

--- backoffice/src/Models/FormModels/DroitForm.cs
+++ backoffice/src/Models/FormModels/DroitForm.cs
@@ -84,12 +84,14 @@ public class DroitForm
     [DisplayName("Wreck Construction Details")]
     public string? WreckConstructionDetails { get; set; } = string.Empty;
 
-    [DisplayName("Is Hazardous Find")] public bool IsHazardousFind { get; set; }
+    [DisplayName("Is Hazardous Find")]
+    public bool IsHazardousFind { get; set; }
 
 
     // Wreck Vessel
 
-    [DisplayName("Wreck Vessel Name")] public string? WreckVesselName { get; set; } = string.Empty;
+    [DisplayName("Wreck Vessel Name")]
+    public string? WreckVesselName { get; set; } = string.Empty;
 
     [DisplayName("Wreck Vessel Year Constructed")]
     public int? WreckVesselYearConstructed { get; set; }
@@ -102,48 +104,62 @@ public class DroitForm
     public string? Latitude { get; set; }
     public string? Longitude { get; set; }
 
-    [DisplayName("In UK Waters?")] public bool InUkWaters { get; set; }
+    [DisplayName("In UK Waters?")]
+    public bool InUkWaters { get; set; }
 
     [DisplayName("Location Radius (Units?)")]
     public int? LocationRadius { get; set; } //Units? Unknown.
 
-    [DisplayName("Depth (Units?)")] public int? Depth { get; set; } //Units? Unknown.
+    [DisplayName("Depth (Units?)")]
+    public int? Depth { get; set; } //Units? Unknown.
 
-    [DisplayName("Location Description")] public string? LocationDescription { get; set; } = string.Empty;
+    [DisplayName("Location Description")]
+    public string? LocationDescription { get; set; } = string.Empty;
 
 
     // Salvage
 
-    [DisplayName("Salvage Award Claimed")] public bool SalvageAwardClaimed { get; set; }
+    [DisplayName("Salvage Award Claimed")]
+    public bool SalvageAwardClaimed { get; set; }
 
-    [DisplayName("Services Description")] public string? ServicesDescription { get; set; }
+    [DisplayName("Services Description")]
+    public string? ServicesDescription { get; set; }
 
-    [DisplayName("Services Duration")] public int? ServicesDuration { get; set; } //Units? Unknown.
+    [DisplayName("Services Duration")]
+    public int? ServicesDuration { get; set; } //Units? Unknown.
 
     [DisplayName("Services Estimated Cost")]
 
     public float? ServicesEstimatedCost { get; set; }
 
-    [DisplayName("MMO Licence Required")] public bool MMOLicenceRequired { get; set; }
+    [DisplayName("MMO Licence Required")]
+    public bool MMOLicenceRequired { get; set; }
 
-    [DisplayName("MMO Licence Provided")] public bool MMOLicenceProvided { get; set; }
+    [DisplayName("MMO Licence Provided")]
+    public bool MMOLicenceProvided { get; set; }
 
-    [DisplayName("Salvage Claim Awarded")] public float SalvageClaimAwarded { get; set; }
+    [DisplayName("Salvage Claim Awarded")]
+    public float SalvageClaimAwarded { get; set; }
 
     // Legacy fields
     public string? District { get; set; }
 
-    [DisplayName("Legacy File Reference")] public string? LegacyFileReference { get; set; } //Physical file location/ref
+    [DisplayName("Legacy File Reference")]
+    public string? LegacyFileReference { get; set; } //Physical file location/ref
 
-    [DisplayName("Goods Discharged By")] public string? GoodsDischargedBy { get; set; } //Initals of RoW member.
+    [DisplayName("Goods Discharged By")]
+    public string? GoodsDischargedBy { get; set; } //Initals of RoW member.
 
-    [DisplayName("Date Delivered")] public string? DateDelivered { get; set; } //Unsure of date format.
+    [DisplayName("Date Delivered")]
+    public string? DateDelivered { get; set; } //Unsure of date format.
 
     public string? Agent { get; set; }
 
-    [DisplayName("Recovered From")] public string? RecoveredFrom { get; set; }
+    [DisplayName("Recovered From")]
+    public string? RecoveredFrom { get; set; }
 
-    [DisplayName("Imported From Legacy")] public bool ImportedFromLegacy { get; set; }
+    [DisplayName("Imported From Legacy")]
+    public bool ImportedFromLegacy { get; set; }
 
     public Droit ApplyChanges(Droit droit)
     {
--- backoffice/src/Models/FormModels/EmailForm.cs
+++ backoffice/src/Models/FormModels/EmailForm.cs
@@ -7,7 +7,8 @@ public class EmailForm
     public string EmailAddress { get; set; } = string.Empty;
     public string Subject { get; set; } = string.Empty;
 
-    [DataType(DataType.MultilineText)] public string Body { get; set; } = string.Empty;
+    [DataType(DataType.MultilineText)]
+    public string Body { get; set; } = string.Empty;
 
     public Dictionary<string, dynamic> GetPersonalisation()
     {
