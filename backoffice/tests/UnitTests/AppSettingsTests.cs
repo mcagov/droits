78a775a6e382ac19fc10445a74a5eedd8608e14d
Sam Kendell <sam.kendell@madetech.com>
2023-06-20 11:23:33 +0100

    appsettings tests

---
+++ backoffice/tests/UnitTests/AppSettingsTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+
+namespace tests
+{
+    public class AppSettingsUnitTests
+    {
+
+    [Fact]
+    public void ReadAppSettingsFile_ShouldReadFileContents()
+    {
+        // Given
+        string filePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+
+        // When
+        string fileContents = File.ReadAllText(filePath);
+
+        // Then
+        Assert.NotEmpty(fileContents);
+        Assert.Equal("", fileContents);
+    }
+
+    }
+}
