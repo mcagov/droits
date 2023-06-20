bc5cae4d554ae17f0838a192ac9e0e112662aa42
Sam Kendell <sam.kendell@madetech.com>
2023-06-20 13:30:12 +0100

    tidy up

--- backoffice/tests/UnitTests/AppSettingsTests.cs
+++ backoffice/tests/UnitTests/AppSettingsTests.cs
@@ -1,22 +1,20 @@
-using Microsoft.Extensions.Configuration;
-
 namespace tests
 {
     public class AppSettingsUnitTests
     {
 
-    [Fact]
-    public void ReadAppSettingsFile_ShouldReadFileContents()
-    {
-        // Given
-        string filePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+        [Fact]
+        public void ReadAppSettingsFile_ShouldReadFileContents()
+        {
+            // Given
+            string filePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
 
-        // When
-        string fileContents = File.ReadAllText(filePath);
+            // When
+            string fileContents = File.ReadAllText(filePath);
 
-        // Then
-        Assert.NotEmpty(fileContents);
-    }
+            // Then
+            Assert.NotEmpty(fileContents);
+        }
 
     }
 }
