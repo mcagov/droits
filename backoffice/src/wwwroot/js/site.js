d46c371623e9ff4eb25596937ee5edb95d54c8f8
Sam Kendell <sam.kendell@madetech.com>
2023-06-26 13:01:16 +0100

    View tweaks, webpack updates

--- backoffice/src/wwwroot/js/site.js
+++ backoffice/src/wwwroot/js/site.js
@@ -1,15 +1,1 @@
 ﻿import 'bootstrap/dist/js/bootstrap.bundle';
-
-// Test if Bootstrap is available
-if (typeof bootstrap !== 'undefined') {
-  console.log('Bootstrap is imported correctly.');
-} else {
-  console.log('Bootstrap is not imported correctly.');
-}
-
-$(document).ready(function() {
-    $('#formTab a').on('click', function (e) {
-      e.preventDefault();
-      $(this).tab('show');
-    });
-  });
