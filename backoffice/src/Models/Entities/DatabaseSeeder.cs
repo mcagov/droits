5a4068df4b4093aa0d550a69813fb5fb13c0b4d4
Sam Kendell <sam.kendell@madetech.com>
2023-06-27 12:50:59 +0100

    Updated seeded wrecks with famous shipwrecks

--- backoffice/src/Models/Entities/DatabaseSeeder.cs
+++ backoffice/src/Models/Entities/DatabaseSeeder.cs
@@ -16,68 +16,76 @@ public static class DatabaseSeeder
     }
 
     private static List<Wreck> GetWrecks() => new List<Wreck>{
-            new Wreck
-            {
-                Id = Guid.NewGuid(),
-                Name = "Great Wall of China",
-                Latitude = "40.431908",
-                Longitude = "116.570374",
-                Created = DateTime.UtcNow,
-                LastModified = DateTime.UtcNow
-            },
-            new Wreck
-            {
-                Id = Guid.NewGuid(),
-                Name = "Petra",
-                Latitude = "30.328611",
-                Longitude = "35.444445",
-                Created = DateTime.UtcNow,
-                LastModified = DateTime.UtcNow
-            },
-            new Wreck
-            {
-                Id = Guid.NewGuid(),
-                Name = "Christ the Redeemer",
-                Latitude = "-22.951944",
-                Longitude = "-43.210556",
-                Created = DateTime.UtcNow,
-                LastModified = DateTime.UtcNow
-            },
-            new Wreck
-            {
-                Id = Guid.NewGuid(),
-                Name = "Machu Picchu",
-                Latitude = "-13.163056",
-                Longitude = "-72.545556",
-                Created = DateTime.UtcNow,
-                LastModified = DateTime.UtcNow
-            },
-            new Wreck
-            {
-                Id = Guid.NewGuid(),
-                Name = "Chichen Itza",
-                Latitude = "20.682778",
-                Longitude = "-88.569167",
-                Created = DateTime.UtcNow,
-                LastModified = DateTime.UtcNow
-            },
-            new Wreck
-            {
-                Id = Guid.NewGuid(),
-                Name = "Colosseum",
-                Latitude = "41.890169",
-                Longitude = "12.492269",
-                Created = DateTime.UtcNow,
-                LastModified = DateTime.UtcNow
-            },
-            new Wreck
-            {
-                Id = Guid.NewGuid(),
-                Name = "Taj Mahal",
-                Latitude = "27.175015",
-                Longitude = "78.042155",
-                Created = DateTime.UtcNow,
-                LastModified = DateTime.UtcNow
-            }
-        };
+        new Wreck
+        {
+            Id = Guid.NewGuid(),
+            Name = "Titanic",
+            Latitude = "41.7325",
+            Longitude = "49.9469",
+            DateOfLoss = new DateTime(1912, 4, 15),
+            Created = DateTime.UtcNow,
+            LastModified = DateTime.UtcNow
+        },
+        new Wreck
+        {
+            Id = Guid.NewGuid(),
+            Name = "Bismarck",
+            Latitude = "48.1667",
+            Longitude = "-16.8667",
+            DateOfLoss = new DateTime(1941, 5, 27),
+            Created = DateTime.UtcNow,
+            LastModified = DateTime.UtcNow
+        },
+        new Wreck
+        {
+            Id = Guid.NewGuid(),
+            Name = "USS Arizona",
+            Latitude = "21.3646",
+            Longitude = "-157.9492",
+            DateOfLoss = new DateTime(1941, 12, 7),
+            Created = DateTime.UtcNow,
+            LastModified = DateTime.UtcNow
+        },
+        new Wreck
+        {
+            Id = Guid.NewGuid(),
+            Name = "HMS Victory",
+            Latitude = "50.7997",
+            Longitude = "-1.1095",
+            DateOfLoss = new DateTime(1744, 10, 5),
+            Created = DateTime.UtcNow,
+            LastModified = DateTime.UtcNow
+        },
+        new Wreck
+        {
+            Id = Guid.NewGuid(),
+            Name = "Andrea Doria",
+            Latitude = "40.4497",
+            Longitude = "-69.6814",
+            DateOfLoss = new DateTime(1956, 7, 25),
+            Created = DateTime.UtcNow,
+            LastModified = DateTime.UtcNow
+        },
+        new Wreck
+        {
+            Id = Guid.NewGuid(),
+            Name = "Birkenhead",
+            Latitude = "-34.5966",
+            Longitude = "19.3605",
+            DateOfLoss = new DateTime(1852, 2, 26),
+            Created = DateTime.UtcNow,
+            LastModified = DateTime.UtcNow
+        },
+        new Wreck
+        {
+            Id = Guid.NewGuid(),
+            Name = "Vasa",
+            Latitude = "59.3293",
+            Longitude = "18.0721",
+            DateOfLoss = new DateTime(1628, 8, 10),
+            Created = DateTime.UtcNow,
+            LastModified = DateTime.UtcNow
+        }
+    };
+
 }
