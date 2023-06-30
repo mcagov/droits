39ff792c7d5882811cbb40d24ee825c47998e2ae
Sam <sam.brown@madetech.com>
2023-06-30 15:13:19 +0100

    Add validation for Phone and Postcode format

--- backoffice/src/Models/FormModels/SalvorForm.cs
+++ backoffice/src/Models/FormModels/SalvorForm.cs
@@ -30,18 +30,24 @@ public class SalvorForm
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Invalid email address")]
     public string Email { get; set; } = string.Empty;
-
+    [Required(ErrorMessage = "Phone number is required")]
+    [Phone()]
     public string TelephoneNumber { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Date of Birth is required")]
     [BindProperty, DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [DataType(DataType.Date)]
     public DateTime DateOfBirth { get; set; } 
-    public string AddressLine1 { get; set; }
-    public string AddressLine2 { get; set; }
-    public string AddressTown { get; set; }
-    public string AddressCounty { get; set; }
-    public string AddressPostcode { get; set; }
+    public string AddressLine1 { get; set; } = string.Empty;
+    public string AddressLine2 { get; set; } = string.Empty;
+    public string AddressTown { get; set; } = string.Empty;
+    public string AddressCounty { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Postcode is required")]
+    [RegularExpression("([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})" +
+                       "|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])" +
+                       "|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2})",
+        ErrorMessage = "Invalid postcode")]
+    public string AddressPostcode { get; set; } = string.Empty;
 
     public Salvor ApplyChanges(Salvor salvor)
     {
