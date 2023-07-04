18e667b7f4b820948ad33fba60ec7b610fdf31f2
Sam <sam.brown@madetech.com>
2023-07-04 09:59:36 +0100

    update postcode regex string

--- backoffice/src/Helpers/Constants.cs
+++ backoffice/src/Helpers/Constants.cs
@@ -2,5 +2,5 @@ namespace Droits.Helpers;
 
 public static class Constants
 {
-    public const string PostcodeRegex = @"^([a-zA-Z]{1,2}\d{1,2}[a-zA-Z]{0,1})\s*?(\d[a-zA-Z]{2})$";
+    public const string PostcodeRegex = @"^(?i)([a-z]{1,2}\d{1,2}[a-z]?)\s?\d[a-z]{2}$";
 }
