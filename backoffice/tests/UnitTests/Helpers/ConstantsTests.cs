414d9856ac39051f914fafab251ba9086a76535f
Sam <sam.brown@madetech.com>
2023-07-04 10:07:33 +0100

    pull out regex to test into top level

--- backoffice/tests/UnitTests/Helpers/ConstantsTests.cs
+++ backoffice/tests/UnitTests/Helpers/ConstantsTests.cs
@@ -6,32 +6,30 @@ namespace Droits.Tests.UnitTests.Helpers;
 
 public class ConstantsTests
 {
+    private Regex regex = new (Constants.PostcodeRegex);
+    
     [Fact]
     public void PostcodeMatch_WithPostcodeWithSpace_ShouldReturnTrue()
     {
         var postcode = "BI9 3LF";
-        var regex = new Regex(Constants.PostcodeRegex);
         Assert.True(regex.Match(postcode).Success);
     }
     [Fact]
     public void PostcodeMatch_WithPostcodeWithoutSpace_ShouldReturnTrue()
     {
         var postcode = "BI93LF";
-        var regex = new Regex(Constants.PostcodeRegex);
         Assert.True(regex.Match(postcode).Success);
     }
     [Fact]
     public void PostcodeMatch_WithPostcodeWithFourthLetterInGroupOne_ShouldReturnTrue()
     {
         var postcode = "SW1A 1AA";
-        var regex = new Regex(Constants.PostcodeRegex);
         Assert.True(regex.Match(postcode).Success);
     }
     [Fact]
     public void PostcodeMatch_WithNotAPostcode_ShouldReturnFalse()
     {
         var postcode = "no good";
-        var regex = new Regex(Constants.PostcodeRegex);
         Assert.False(regex.Match(postcode).Success);
     }
 }
