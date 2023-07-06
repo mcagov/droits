9157fe4233e4f515f4519ab26b36ad1afe215506
Sam <sam.brown@madetech.com>
2023-07-06 11:07:04 +0100

    Update Droit Status Enum values

--- backoffice/src/Models/Enums/DroitStatus.cs
+++ backoffice/src/Models/Enums/DroitStatus.cs
@@ -1,24 +1,31 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
 
 namespace Droits.Models.Enums;
 
 public enum DroitStatus
 {
-    [Display(Name = "Unassigned")]
-    Unassigned,
-
     [Display(Name = "Received")]
     Received,
-
-    [Display(Name = "In Progress")]
-    InProgress,
+    
+    [Display(Name = "Acknowledged")]
+    AcknowledgementLetterSent,
+    
+    [Display(Name = "Requesting Info")]
+    RequestingInformation,
 
     [Display(Name = "Research")]
     Research,
+    
+    [Display(Name = "Salvage Award")]
+    NegotiatingSalvageAward,
+    
+    [Display(Name = "Ready For QC")]
+    ReadyForQc,
+    
+    [Display(Name = "QC Approved")]
+    QcApproved,
 
     [Display(Name = "Closed")]
     Closed,
-
-    [Display(Name = "Awaiting Response")]
-    AwaitingResponse,
 }
