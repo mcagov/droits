387d32ca41c85f145f8196058d917d665d0686b2
Sam <sam.brown@madetech.com>
2023-07-25 12:14:00 +0100

    Refactor tests

--- backoffice/tests/UnitTests/Letter/LetterPersonalisationViewUnitTests.cs
+++ backoffice/tests/UnitTests/Letter/LetterPersonalisationViewUnitTests.cs
@@ -5,9 +5,8 @@ namespace Droits.Tests.UnitTests.Letter;
 
 public class LetterPersonalisationViewUnitTests
 {
-    //Add tests here for SubstituteLetterContentWithParamsAsync// SamB
     [Fact]
-    public async void SubstituteLetterContentWithParamsAsync_ShouldReturnCorrectLetterContent()
+    public void ShouldReturnCorrectLetterContent()
     {
         Wreck wreck = new()
         {
@@ -16,14 +15,13 @@ public class LetterPersonalisationViewUnitTests
         Droit droit = new()
         {
             Reference = "111/11",
-            ReportedDate = new DateTime(1971,7,14),
+            ReportedDate = new DateTime(1971, 7, 14),
             Wreck = wreck
         };
 
         var letterView = new LetterPersonalisationView(droit);
         var response = letterView.SubstituteContent("((reference)) ((wreck)) ((foobar)) ((date))");
-        
-        Assert.Equal("111/11 Sinky ((foobar)) 14/07/1971",response);
-    }
 
+        Assert.Equal("111/11 Sinky ((foobar)) 14/07/1971", response);
+    }
 }
