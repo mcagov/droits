867306b89d76984fbae41ec14e9c01c9149cb155
Sam Kendell <sam.kendell@madetech.com>
2023-08-15 12:08:17 +0100

    tidy up and update launch settings for https

--- backoffice/src/Program.cs
+++ backoffice/src/Program.cs
@@ -24,7 +24,6 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
         builder.Configuration.Bind("AzureAd", options);
     });
 
-
 builder.Services
     .AddControllersWithViews(options =>
     {
@@ -43,12 +42,16 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
     options.DefaultRequestCulture = new RequestCulture("en-GB");
 });
 
-
 builder.Services.AddDbContext<DroitsContext>(opt => opt.UseInMemoryDatabase("droits"));
 
 builder.Services.AddSession();
 builder.Services.AddHealthChecks();
+
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+builder.Services.AddScoped<ITokenValidationService, TokenValidationService>();
+
+builder.Services.AddScoped<IGovNotifyClient, GovNotifyClient>();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -58,8 +61,6 @@ builder.Services.AddScoped<INoteService, NoteService>();
 
 builder.Services.AddScoped<ILetterRepository, LetterRepository>();
 builder.Services.AddScoped<ILetterService, LetterService>();
-
-builder.Services.AddScoped<IGovNotifyClient, GovNotifyClient>();
 
 builder.Services.AddScoped<IDroitRepository, DroitRepository>();
 builder.Services.AddScoped<IWreckMaterialRepository, WreckMaterialRepository>();
@@ -72,49 +73,57 @@ builder.Services.AddScoped<ISalvorRepository, SalvorRepository>();
 builder.Services.AddScoped<ISalvorService, SalvorService>();
 
 
-builder.Services.AddScoped<ITokenValidationService, TokenValidationService>();
-
-
 builder.Services.AddGovUkFrontend();
 
 builder.Logging.AddConsole();
-
 builder.Services.AddLogging(loggingBuilder =>
 {
     loggingBuilder.AddConsole();
     loggingBuilder.SetMinimumLevel(LogLevel.Debug);
 });
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.Secure = CookieSecurePolicy.Always;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
 
 var app = builder.Build();
 
-app.MapHealthChecks("/healthz").AllowAnonymous();
-
-// Configure the HTTP request pipeline.
-if ( !app.Environment.IsDevelopment() )
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-using ( var scope = app.Services.CreateScope() )
+app.UseHttpsRedirection();
+
+using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<DroitsContext>();
     DatabaseSeeder.SeedData(dbContext);
 }
+
 app.UseRequestLocalization();
 app.UseSession();
-
+app.UseCookiePolicy();
 app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthentication();
-
 app.UseMiddleware<TokenValidationMiddleware>();
-
 app.UseAuthorization();
-
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
