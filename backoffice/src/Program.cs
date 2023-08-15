52a3170c36060ad34b590c25fac24200babb410b
Sam Kendell <sam.kendell@madetech.com>
2023-08-15 14:15:46 +0100

    testing https

--- backoffice/src/Program.cs
+++ backoffice/src/Program.cs
@@ -16,37 +16,43 @@ using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ** CONFIGURATION **
+
+// HttpContext Access
 builder.Services.AddHttpContextAccessor();
 
+// Authentication and Authorization
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(options =>
     {
         builder.Configuration.Bind("AzureAd", options);
-    });
-
-builder.Services
+    })
     .AddControllersWithViews(options =>
     {
         options.ModelBinderProviders.Insert(0, new DateTimeModelBinderProvider());
-        
+
         var policy = new AuthorizationPolicyBuilder()
             .RequireAuthenticatedUser()
             .Build();
-        
+
         options.Filters.Add(new AuthorizeFilter(policy));
     })
     .AddRazorRuntimeCompilation().AddMicrosoftIdentityUI().AddSessionStateTempDataProvider();
 
+// Localization
 builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
     options.DefaultRequestCulture = new RequestCulture("en-GB");
 });
 
+// Database Context
 builder.Services.AddDbContext<DroitsContext>(opt => opt.UseInMemoryDatabase("droits"));
 
+// Session and HealthChecks
 builder.Services.AddSession();
 builder.Services.AddHealthChecks();
 
+// Dependency Injections
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 
 builder.Services.AddScoped<ITokenValidationService, TokenValidationService>();
@@ -72,21 +78,25 @@ builder.Services.AddScoped<IWreckService, WreckService>();
 builder.Services.AddScoped<ISalvorRepository, SalvorRepository>();
 builder.Services.AddScoped<ISalvorService, SalvorService>();
 
-
+// GovUK Frontend
 builder.Services.AddGovUkFrontend();
 
-builder.Logging.AddConsole();
+// Logging
 builder.Services.AddLogging(loggingBuilder =>
 {
     loggingBuilder.AddConsole();
     loggingBuilder.SetMinimumLevel(LogLevel.Debug);
 });
 
+// Forwarded Headers
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
 });
 
+// Cookie Policy
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
     options.CheckConsentNeeded = context => true;
@@ -100,20 +110,25 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
 });
 
+// ** APP BUILD & MIDDLEWARE **
+
 var app = builder.Build();
 
+// Error handling
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
 }
 
+// Seeding the database
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<DroitsContext>();
     DatabaseSeeder.SeedData(dbContext);
 }
 
+// Middleware Pipeline
+app.UseForwardedHeaders();
 app.UseRequestLocalization();
 app.UseSession();
 app.UseCookiePolicy();
@@ -122,11 +137,8 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseMiddleware<TokenValidationMiddleware>();
 app.UseAuthorization();
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
 
+// Routing
 app.MapControllerRoute(
     "default",
     "{controller=Home}/{action=Index}/{id?}");
