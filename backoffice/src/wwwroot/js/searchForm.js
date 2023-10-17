16df8655eb71a13b60c47a4c944252efd7c00f4c
Sam <sam.brown@madetech.com>
2023-10-17 09:41:37 +0100

    Remove unused js

--- backoffice/src/wwwroot/js/searchForm.js
+++ backoffice/src/wwwroot/js/searchForm.js
@@ -2,37 +2,22 @@ import Choices from "choices.js";
 
 
 export function initializeSearchForm() {
-    // Initialize your Choices dropdowns
-    const statusDropdown = new Choices('.js-search-droit-status', {
+
+    new Choices('.js-search-droit-status', {
         removeItems: true,
         removeItemButton: true,
         placeholder: true,
         placeholderValue: 'Select status',
     });
 
-    const userDropdown = new Choices('.js-select-assigned-user', {
+    new Choices('.js-select-assigned-user', {
         allowHTML: false,
         searchEnabled: true,
         itemSelectText: 'Select',
     });
 
-    // Attach a click event listener to each dropdown
-    statusDropdown.passedElement.element.addEventListener('click', () => {
-        bringDropdownToFront(statusDropdown.passedElement.element);
-    });
-
-    userDropdown.passedElement.element.addEventListener('click', () => {
-        bringDropdownToFront(userDropdown.passedElement.element);
-    });
 }
 
-function bringDropdownToFront(dropdownElement) {
-    // Find the parent element of the dropdown
-    const parent = dropdownElement.parentElement;
-
-    // Move the dropdown to the end of the parent's child elements
-    parent.appendChild(dropdownElement);
-}
 
 
 
