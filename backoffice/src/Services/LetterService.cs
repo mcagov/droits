46f2fde4932591a84786bc5609e15e0f777f4a52
Sam Kendell <sam.kendell@madetech.com>
2023-11-08 15:28:47 +0000

    Moved letter content functions to helper class

--- backoffice/src/Services/LetterService.cs
+++ backoffice/src/Services/LetterService.cs
@@ -312,8 +312,9 @@ public class LetterService : ILetterService
             throw new DroitNotFoundException();
         }
         
+        var templateBody = await GetTemplateBodyAsync(LetterType.ReportConfirmed, droit);
 
-        confirmationLetter.Body = await GetReportConfirmedEmailBodyAsync(droit, report);
+        confirmationLetter.Body = LetterContentHelper.GetReportConfirmedEmailBodyAsync(droit, report, templateBody);
 
         confirmationLetter = await _repo.AddAsync(confirmationLetter);
         await SendLetterAsync(confirmationLetter.Id); // Don't actually send it at the moment 
@@ -322,63 +323,4 @@ public class LetterService : ILetterService
         _logger.LogInformation(
             $"Sending confirmation email to {salvor?.Email} for Droit {droit?.Reference}");
     }
-private async Task<string> GetReportConfirmedEmailBodyAsync(Droit droit, SubmittedReportDto report)
-{
-    var templateBody = await GetTemplateBodyAsync(LetterType.ReportConfirmed, droit);
-
-    var submittedWreckName = report.VesselName.ValueOrEmpty();
-
-    var itemsReportedSection = BuildItemsReportedSection(droit, submittedWreckName ?? string.Empty);
-
-    templateBody = Regex.Replace(templateBody, @"\(\(items_reported_section\)\)", itemsReportedSection);
-
-    var isLateSubmission = true || droit.ReportedDate.Subtract(droit.DateFound).Days > 28;
-    var lateReportSection = BuildLateReportSection(isLateSubmission);
-
-    templateBody = Regex.Replace(templateBody, @"\(\(late_report_section\)\)", lateReportSection);
-
-    var yourResponsibilitiesSection = BuildYourResponsibilitiesSection(droit.WreckMaterials.Count);
-
-    templateBody = Regex.Replace(templateBody, @"\(\(your_responsibilities_section\)\)", yourResponsibilitiesSection);
-
-    return templateBody;
-}
-
-private static string BuildItemsReportedSection(Droit droit, string submittedWreckName)
-{
-    var itemsReportedSection = $@"
-        The following {"item has".Pluralize(droit.WreckMaterials.Count, "items have")} been reported {(!string.IsNullOrEmpty(submittedWreckName) ? $"from the wreck named {submittedWreckName}" : "")} :
-";
-    itemsReportedSection += " \n";
-    itemsReportedSection = droit.WreckMaterials.Aggregate(itemsReportedSection, (current, item) => current + $"\n - {item.Name}");
-
-    return itemsReportedSection;
-}
-
-private static string BuildLateReportSection(bool isLateSubmission)
-{
-    var lateReportSection = "";
-
-    if ( !isLateSubmission ) return lateReportSection;
-    
-    lateReportSection += @"#Report submitted late";
-    lateReportSection += @"
-            You have not submitted this Report of wreck material within 28 days of the wreck material being recovered. For all future recoveries, please note that all recovered wreck material should be reported to the Receiver of Wreck within 28 days of recovery.";
-
-    return lateReportSection;
-}
-
-private static string BuildYourResponsibilitiesSection(int itemCount)
-{
-    var yourResponsibilitiesSection = $@"
-            The recovered {"item is".Pluralize(itemCount, "items are")} now held by you on indemnity to the Receiver of Wreck whilst investigations into legal ownership are undertaken.
-
-            You do not own the reported {"item".Pluralize(itemCount, "items")} until advised by the Receiver of Wreck. You must not sell or otherwise dispose of the {"item".Pluralize(itemCount, "items")} until advised by the Receiver of Wreck.
-
-            All finds from a maritime context are subject to various physical and chemical processes once raised. Conservators advise that finds should be kept wet, cool and dark until more specific advice can be sought.
-";
-
-    return yourResponsibilitiesSection;
-}
-
 }
