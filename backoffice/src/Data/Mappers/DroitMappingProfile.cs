6c0758126ad2e73663413c570e6d40f32a42e0dc
Sam Kendell <sam.kendell@madetech.com>
2023-11-10 11:14:26 +0000

    Mapping droit fields

--- backoffice/src/Data/Mappers/DroitMappingProfile.cs
+++ backoffice/src/Data/Mappers/DroitMappingProfile.cs
@@ -10,11 +10,38 @@ namespace Droits.Data.Mappers
         public DroitMappingProfile()
         {
             CreateMap<SubmittedReportDto, Droit>()
+                .ForMember(dest => dest.ReportedDate,
+                    opt => opt.MapFrom(src =>
+                        src.ReportDate == null ? DateTime.UtcNow : src.ReportDate.AsDateTime()))
+                .ForMember(dest => dest.DateFound,
+                    opt => opt.MapFrom(src =>
+                        src.WreckFindDate == null ? DateTime.UtcNow : src.WreckFindDate.AsDateTime()))
+                .ForMember(dest => dest.Latitude,
+                    opt => opt.MapFrom(src =>
+                        src.Latitude))
+                .ForMember(dest => dest.Longitude,
+                    opt => opt.MapFrom(src =>
+                        src.Longitude))
+                .ForMember(dest => dest.LocationRadius,
+                    opt => opt.MapFrom(src =>
+                        src.LocationRadius))
                 .ForMember(dest => dest.LocationDescription,
                     opt => opt.MapFrom(src =>
                         src.LocationDescription == null
                             ? string.Empty
                             : src.LocationDescription.ValueOrEmpty()))
+                .ForMember(dest => dest.RecoveredFrom,
+                    opt => opt.MapFrom(src =>
+                        src.RemovedFrom != null ? src.RemovedFrom.Replace(" ", "").ToLower() : null))
+                .ForMember(dest => dest.RecoveredFromLegacy,
+                    opt => opt.MapFrom(src =>
+                        src.RemovedFrom))
+                .ForMember(dest => dest.SalvageAwardClaimed,
+                    opt => opt.MapFrom(src =>
+                        src.ClaimSalvage))
+                .ForMember(dest => dest.ServicesDescription,
+                    opt => opt.MapFrom(src =>
+                        src.SalvageServices))
                 .ForMember(dest => dest.WreckMaterials, opt => opt.Ignore());
 
         }
