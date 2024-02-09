0aa40c6a993c9cc734ad48eb38e4417fab93f7fa
Sam <sam.brown@madetech.com>
2024-02-09 11:54:35 +0000

    Test adding static login page

--- webapp/server.js
+++ webapp/server.js
@@ -103,6 +103,10 @@ app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, './dist')));
 app.use('/uploads', express.static('uploads'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/auth', express.static(path.join(__dirname, 'app', 'auth')));
+
+
 app.use(
   '/assets',
   express.static(
