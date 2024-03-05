6f2b40a802d373bd1426b4fcca98011995b6db5e
Sam Kendell <sam.kendell@madetech.com>
2024-03-05 12:24:58 +0000

    updated redis param group

--- terraform/modules/elasticache/main.tf
+++ terraform/modules/elasticache/main.tf
@@ -8,7 +8,7 @@ resource "aws_elasticache_cluster" "main" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis7.x"
+  parameter_group_name = "default.redis7"
   engine_version       = "7.x"
   port                 = var.redis_port
   subnet_group_name    = aws_elasticache_subnet_group.main.name
