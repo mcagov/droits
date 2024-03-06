ab362b6dfdda817bdd6e2985d6ad90cd8fc32a46
Sam <sam.brown@madetech.com>
2024-03-06 11:21:02 +0000

    bump up redis version

--- terraform/modules/elasticache/main.tf
+++ terraform/modules/elasticache/main.tf
@@ -9,7 +9,7 @@ resource "aws_elasticache_cluster" "main" {
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7"
-  engine_version       = "7.x"
+  engine_version       = "7.1"
   port                 = var.redis_port
   subnet_group_name    = aws_elasticache_subnet_group.main.name
   security_group_ids   = var.security_groups
