584ad7b3f33170939aeb9bec9ee7391af6d68998
Sam Kendell <sam.kendell@madetech.com>
2024-04-23 16:03:30 +0100

    fixed merge issue

--- backoffice/src/Models/DTOs/Imports/AccessDto.cs
+++ backoffice/src/Models/DTOs/Imports/AccessDto.cs
@@ -231,9 +231,6 @@ public class AccessDto
             new string(input.Where(c => char.IsDigit(c) || c == '-' || c == '.').ToArray());
 
         if ( string.IsNullOrEmpty(cleanedDepth) )
-        var cleanedDepth = new string(Depth.Where(c => char.IsDigit(c) || c == '-' || c == '.').ToArray());
-
-        if ( string.IsNullOrEmpty(cleanedDepth) )
         {
             return null;
         }
