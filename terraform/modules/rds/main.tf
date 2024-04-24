f99ea78f6a4b7e4614896a10642700256b62e03b
Sam Kendell <sam.kendell@madetech.com>
2024-04-24 17:52:11 +0100

    increased prod capacity, added backups for all envs.

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -9,20 +9,21 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 }
 
 resource "aws_db_instance" "droits" {
-  allocated_storage      = var.db_allocated_storage
-  db_name                = var.db_name
-  identifier             = "${terraform.workspace}-db"
-  engine                 = "postgres"
-  engine_version         = "14"
-  instance_class         = var.db_instance_class
-  username               = var.db_username
-  password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
-  vpc_security_group_ids = var.db_security_groups
-  deletion_protection    = var.db_delete_protection
-  parameter_group_name   = "default.postgres14"
-  skip_final_snapshot    = true
-  storage_encrypted      = true
+  allocated_storage       = var.db_allocated_storage
+  db_name                 = var.db_name
+  identifier              = "${terraform.workspace}-db"
+  engine                  = "postgres"
+  engine_version          = "14"
+  instance_class          = var.db_instance_class
+  username                = var.db_username
+  password                = var.db_password
+  db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids  = var.db_security_groups
+  deletion_protection     = var.db_delete_protection
+  parameter_group_name    = "default.postgres14"
+  skip_final_snapshot     = false
+  storage_encrypted       = true
+  backup_retention_period = 14
 
   lifecycle {
     create_before_destroy = true
