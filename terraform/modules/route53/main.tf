bc63b6b0ba23e8806c379dd4b1d7030ea9fc3835
bnjn-mt <benjamin.johnson@madetech.com>
2024-08-19 16:37:09 +0100

    Updated route53 module to dynamically create records from lists and validate ssl cert using dns

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -1,147 +1,31 @@
 resource "aws_route53_zone" "report_wreck_material" {
-  name  = var.root_domain_name
+  name = var.root_domain_name
 }
 
-resource "aws_route53_record" "production_webapp" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = var.root_domain_name
-  type    = "A"
+resource "aws_route53_record" "dns_a_records" {
+  for_each = { for record in var.a_records : record.name => record }
+  name     = each.value.name
+  type     = "A"
+  ttl      = 300
+  records  = [each.value.alb_dns]
+  zone_id  = aws_route53_zone.report_wreck_material.zone_id
 
-  alias {
-    name                   = var.production_webapp_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
+  depends_on = [aws_route53_zone.report_wreck_material]
 }
 
-resource "aws_route53_record" "production_webapp_www" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = "www.${var.root_domain_name}"
-  type    = "A"
+resource "aws_route53_record" "dns_ssl_validation" {
+  for_each = { for dvo in var.domain_validation_options : dvo.domain_name => dvo }
 
-  alias {
-    name                   = var.production_webapp_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  ttl     = 60
+  records = [each.value.resource_record_value]
+  zone_id = aws_route53_zone.report_wreck_material.zone_id
 }
 
-resource "aws_route53_record" "production_webapp_subdomain" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = "webapp.${var.root_domain_name}"
-  type    = "A"
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = var.ssl_certificate_arn
+  validation_record_fqdns = [for record in aws_route53_record.dns_ssl_validation : record.fqdn]
 
-  alias {
-    name                   = var.production_webapp_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "production_backoffice" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = "backoffice.${var.root_domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = var.production_backoffice_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "staging_webapp" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = "staging.${var.root_domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = var.staging_webapp_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "staging_backoffice" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = "staging.backoffice.${var.root_domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = var.staging_backoffice_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "dev_webapp" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = "dev.${var.root_domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = var.dev_webapp_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "dev_backoffice" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = "dev.backoffice.${var.root_domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = var.dev_backoffice_alb_dns
-    zone_id                = aws_route53_zone.report_wreck_material.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "production_webapp_ssl_verification" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = var.production_webapp_ssl_verification_name
-  type    = "CNAME"
-  ttl     = 300
-  records = [var.production_webapp_ssl_verification_value]
-}
-
-resource "aws_route53_record" "production_backoffice_ssl_verification" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = var.production_backoffice_ssl_verification_name
-  type    = "CNAME"
-  ttl     = 300
-  records = [var.production_backoffice_ssl_verification_value]
-}
-
-resource "aws_route53_record" "staging_webapp_ssl_verification" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = var.staging_webapp_ssl_verification_name
-  type    = "CNAME"
-  ttl     = 300
-  records = [var.staging_webapp_ssl_verification_value]
-}
-
-resource "aws_route53_record" "staging_backoffice_ssl_verification" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = var.staging_backoffice_ssl_verification_name
-  type    = "CNAME"
-  ttl     = 300
-  records = [var.staging_backoffice_ssl_verification_value]
-}
-
-resource "aws_route53_record" "dev_webapp_ssl_verification" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = var.dev_webapp_ssl_verification_name
-  type    = "CNAME"
-  ttl     = 300
-  records = [var.dev_webapp_ssl_verification_value]
-}
-
-resource "aws_route53_record" "dev_backoffice_ssl_verification" {
-  zone_id = aws_route53_zone.report_wreck_material.zone_id
-  name    = var.dev_backoffice_ssl_verification_name
-  type    = "CNAME"
-  ttl     = 300
-  records = [var.dev_backoffice_ssl_verification_value]
+  depends_on = [aws_route53_record.dns_ssl_validation]
 }
--- terraform/modules/route53/variables.tf
+++ terraform/modules/route53/variables.tf
@@ -3,109 +3,19 @@ variable "root_domain_name" {
   description = "The root domain name for DROITS"
 }
 
-variable "create_hosted_zone" {
-  description = "Flag to create hosted zone"
-  type        = bool
+variable "a_records" {
+  type = list(object({
+    name    = string
+    alb_dns = string
+  }))
+  description = "List of A records for Route 53 DNS"
 }
 
-variable "production_webapp_alb_dns" {
-  description = "The DNS name of the production webapp ALB"
+variable "ssl_certificate_arn" {
   type        = string
+  description = "Load Balancer ssl certificate arn"
 }
 
-variable "production_backoffice_alb_dns" {
-  description = "The DNS name of the production backoffice ALB"
-  type        = string
-}
-
-variable "staging_webapp_alb_dns" {
-  description = "The DNS name of the staging webapp ALB"
-  type        = string
-}
-
-variable "staging_backoffice_alb_dns" {
-  description = "The DNS name of the staging backoffice ALB"
-  type        = string
-}
-
-variable "dev_webapp_alb_dns" {
-  description = "The DNS name of the dev webapp ALB"
-  type        = string
-}
-
-variable "dev_backoffice_alb_dns" {
-  description = "The DNS name of the dev backoffice ALB"
-  type        = string
-}
-
-variable "production_webapp_ssl_verification_name" {
-  description = "DNS validation name for production webapp"
-  sensitive   = true
-  type        = string
-}
-
-variable "production_backoffice_ssl_verification_name" {
-  description = "DNS validation name for production backoffice"
-  sensitive   = true
-  type        = string
-}
-
-variable "staging_webapp_ssl_verification_name" {
-  description = "DNS validation name for staging webapp"
-  sensitive   = true
-  type        = string
-}
-
-variable "staging_backoffice_ssl_verification_name" {
-  description = "DNS validation name for staging backoffice"
-  sensitive   = true
-  type        = string
-}
-
-variable "dev_webapp_ssl_verification_name" {
-  description = "DNS validation name for dev webapp"
-  sensitive   = true
-  type        = string
-}
-
-variable "dev_backoffice_ssl_verification_name" {
-  description = "DNS validation name for dev backoffice"
-  sensitive   = true
-  type        = string
-}
-
-variable "production_webapp_ssl_verification_value" {
-  description = "DNS validation value for production webapp"
-  sensitive   = true
-  type        = string
-}
-
-variable "production_backoffice_ssl_verification_value" {
-  description = "DNS validation value for production backoffice"
-  sensitive   = true
-  type        = string
-}
-
-variable "staging_webapp_ssl_verification_value" {
-  description = "DNS validation value for staging webapp"
-  sensitive   = true
-  type        = string
-}
-
-variable "staging_backoffice_ssl_verification_value" {
-  description = "DNS validation value for staging backoffice"
-  sensitive   = true
-  type        = string
-}
-
-variable "dev_webapp_ssl_verification_value" {
-  description = "DNS validation value for dev webapp"
-  sensitive   = true
-  type        = string
-}
-
-variable "dev_backoffice_ssl_verification_value" {
-  description = "DNS validation value for dev backoffice"
-  sensitive   = true
-  type        = string
+variable "domain_validation_options" {
+  description = "Domain validation options for ssl certificate"
 }
