5e69782c83e29b9fad9e5ab22544c3b9adec7cd8
bnjn-mt <benjamin.johnson@madetech.com>
2024-08-22 10:21:22 +0100

    Fixed issue with terraform creating duplicate ssl validation records

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -13,6 +13,9 @@ resource "aws_acm_certificate" "ssl_cert" {
 }
 
 resource "aws_acm_certificate" "imported_certificate" {
-  domain_name               = var.ssl_domains[0]
-  subject_alternative_names = slice(var.ssl_domains, 1, length(var.ssl_domains))
+  domain_name = "*.report-wreck-material.service.gov.uk"
+  subject_alternative_names = [
+    "*.backoffice.report-wreck-material.service.gov.uk",
+    "report-wreck-material.service.gov.uk"
+  ]
 }
