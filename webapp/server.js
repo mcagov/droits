fad12ee3863bb1ceb953592021be6fb91e1c62e8
Alice Lee <alice.lee@digital.cabinet-office.gov.uk>
2024-09-16 15:26:19 +0100

    Don't enable other routes when in service unavailable mode
    Also tidy up server.js a bit

--- webapp/server.js
+++ webapp/server.js
@@ -17,22 +17,9 @@ var connect_redis = require("connect-redis");
 
 var cors = require('cors')
 
-const lusca = require('lusca');
-
-
-// import helmet from 'helmet';
-// import { NONAME } from 'dns';
-
 require("dotenv-json")();
 const app = express();
 app.options('*', cors());
-
-// app.use(lusca.csrf());
-
-// app.use((req, res, next) => {
-//   res.locals.csrfToken = req.csrfToken();
-//   next();
-// });
 
 const PORT = process.env.PORT || config.PORT;
 
@@ -43,7 +30,6 @@ app.locals.serviceName = config.SERVICE_NAME;
 const env = process.env.NODE_ENV;
 
 if (env === 'production') {
-  // app.use(helmet());
   app.get('/', function(req, res, next){
     if(req.hostname === 'report-wreck-material.service.gov.uk'){
       res.redirect('https://www.gov.uk/report-wreck-material/reporting-wreck-material');
@@ -71,120 +57,126 @@ if (isSecure) {
   app.use(forceHttps);
 }
 
-// Support for parsing data in POSTs
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-app.set('trust proxy', 1); // needed for secure cookies on heroku
-
-// Configure nunjucks environment
-const nunjucksAppEnv = nunjucks.configure(
-  [
-    path.join(__dirname, './node_modules/govuk-frontend/'),
-    path.join(__dirname, './app/views/'),
-  ],
-  {
-    autoescape: false,
-    express: app,
-    watch: env === 'development' ? true : false,
-  }
-);
-addCheckedFunction(nunjucksAppEnv);
-addNunjucksFilters(nunjucksAppEnv);
-
-// Set views engine
-app.set('view engine', 'html');
-
-app.use(express.static(path.join(__dirname, './dist')));
-app.use('/uploads', express.static('uploads'));
-app.use('/auth', express.static(path.join(__dirname, 'app', 'static', 'auth')));
-
-
-app.use(
-  '/assets',
-  express.static(
-    path.join(__dirname, './node_modules/govuk-frontend/govuk/assets')
-  )
-);
-
-// Serve CSS files specifically
-app.use('/assets/css', (req, res, next) => {
-  res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-  next();
-}, express.static(path.join(__dirname, './dist/assets/css')));
-
-
-// Session uses service name to avoid clashes with other prototypes
-app.use(session({
-  secret: process.env.CSRFT_SESSION_SECRET,
-  store: new redisStore({
-      client: redisClient,
-      ttl: 3600
-  }),
-  sameSite: 'none',
-  saveUninitialized: false,
-  resave: false,
-  cookie: { httpOnly: true , secure: isSecure},
-  unset: 'destroy'
-}));
-
-// Manage session data. Assigns default values to data
-app.use(sessionData);
-
-// Logs req.session data
-if (env === 'development') edt(app, { panels: ['session'] });
-
-
-app.get('/', function(req, res){
-  res.render('index');
-})
-
-// Load API routes
-app.use('/', routes());
-
-// Disables caching when user clicks back button on confirmation page
-app.use('/report/check-your-answers', function (req, res, next) {
-  res.set(
-    'Cache-Control',
-    'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
-  );
-  next();
-});
-
-app.get(/^([^.]+)$/, function (req, res, next) {
-  if (config.SERVICE_UNAVAILABLE === true) {
+if (config.SERVICE_UNAVAILABLE) {
+  app.get(/^([^.]+)$/, (req, res, next) => {
     console.log('Service Unavailable.');
     res.status('503');
     res.sendFile(path.join(__dirname, '/app/static/service-unavailable.html'));
-  } else {
-    matchRoutes(req, res, next);
-  }
-});
-// Redirect all POSTs to GETs
-app.post(/^\/([^.]+)$/, function (req, res) {
-  res.redirect('/' + req.params[0]);
-});
+  })
+} else {
+  // Support for parsing data in POSTs
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error(`Page not found: ${req.path}`);
-  err.status = 404;
+  // Needed for secure cookies when running behind a proxy
+  // https://expressjs.com/en/guide/behind-proxies.html
+  app.set('trust proxy', 1);
 
-  next(err);
-});
+  // Configure nunjucks environment
+  const nunjucksAppEnv = nunjucks.configure(
+    [
+      path.join(__dirname, './node_modules/govuk-frontend/'),
+      path.join(__dirname, './app/views/'),
+    ],
+    {
+      autoescape: false,
+      express: app,
+      watch: env === 'development' ? true : false,
+    }
+  );
+  addCheckedFunction(nunjucksAppEnv);
+  addNunjucksFilters(nunjucksAppEnv);
 
-// Display error
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
+  // Set views engine
+  app.set('view engine', 'html');
 
-  if (err.message.indexOf('not found') > 0) {
-    res.status(404).render('404');
-  }
-});
+  app.use(express.static(path.join(__dirname, './dist')));
+  app.use('/uploads', express.static('uploads'));
+  app.use('/auth', express.static(path.join(__dirname, 'app', 'static', 'auth')));
+
+
+  app.use(
+    '/assets',
+    express.static(
+      path.join(__dirname, './node_modules/govuk-frontend/govuk/assets')
+    )
+  );
+
+  // Serve CSS files specifically
+  app.use('/assets/css', (req, res, next) => {
+    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    next();
+  }, express.static(path.join(__dirname, './dist/assets/css')));
+
+
+  // Session uses service name to avoid clashes with other prototypes
+  app.use(session({
+    secret: process.env.CSRFT_SESSION_SECRET,
+    store: new redisStore({
+        client: redisClient,
+        ttl: 3600
+    }),
+    sameSite: 'none',
+    saveUninitialized: false,
+    resave: false,
+    cookie: { httpOnly: true , secure: isSecure},
+    unset: 'destroy'
+  }));
+
+  // Manage session data. Assigns default values to data
+  app.use(sessionData);
+
+  // Logs req.session data
+  if (env === 'development') edt(app, { panels: ['session'] });
+
+
+  app.get('/', function(req, res){
+    res.render('index');
+  })
+
+  // Load API routes
+  app.use('/', routes());
+
+  // Disables caching when user clicks back button on confirmation page
+  app.use('/report/check-your-answers', function (req, res, next) {
+    res.set(
+      'Cache-Control',
+      'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+    );
+    next();
+  });
+
+  app.get(/^([^.]+)$/, function (req, res, next) {
+      matchRoutes(req, res, next);
+    }
+  )
+
+  // Redirect all POSTs to GETs
+  app.post(/^\/([^.]+)$/, function (req, res) {
+    res.redirect('/' + req.params[0]);
+  });
+
+  // Catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    var err = new Error(`Page not found: ${req.path}`);
+    err.status = 404;
+
+    next(err);
+  });
+
+  // Display error
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+
+    if (err.message.indexOf('not found') > 0) {
+      res.status(404).render('404');
+    }
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT} - url: http://localhost:${PORT}`);
