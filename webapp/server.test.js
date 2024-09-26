9584c19c6dfab1a3f1a1e46dc713c8197f0aa1cd
bnjn-mt <benjamin.johnson@madetech.com>
2024-09-26 10:14:28 +0100

    Refactored server.js test

--- webapp/server.test.js
+++ webapp/server.test.js
@@ -2,8 +2,10 @@ import request from 'supertest';
 import app from './server';
 
 describe('GET /', () => {
-    it('responds with status 200', async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toEqual(200);
+    it('responds with status 200', () => {
+        request(app).get('/').expect(200)
+        .end((err) => {
+            if (err) throw err;
+        });
     });
 });
