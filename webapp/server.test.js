308e15ab9aedc1738c7aed3bc48b984432b1495c
bnjn-mt <benjamin.johnson@madetech.com>
2024-10-17 11:58:52 +0100

    Added status code 503 test for service shutter for when config SERVICE_UNAVAILABLE is true

--- webapp/server.test.js
+++ webapp/server.test.js
@@ -1,11 +1,33 @@
 import request from 'supertest';
 import app from './server';
+import config from './app/config';
 
 describe('GET /', () => {
     it('responds with status 200', () => {
         request(app).get('/').expect(200)
         .end((err) => {
             if (err) throw err;
+        });
+    });
+});
+
+describe('Service Shutter Tests', () => {
+    
+    describe('SERVICE_UNAVAILABLE is true', () => {
+        beforeAll(() => {
+            jest.mock('./app/config');
+            config.SERVICE_UNAVAILABLE = true;
+        });
+        
+        afterAll(() => {
+            jest.restoreAllMocks();
+        });
+        
+        it('should return 503', () => {
+            request(app).get('/test-route').expect(503)
+            .end((err) => {
+                if (err) throw err;
+            });
         });
     });
 });
