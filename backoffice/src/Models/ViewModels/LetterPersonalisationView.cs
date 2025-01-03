9c143cb5786e29a348a760b804a1534a166806f5
bnjn-mt <benjamin.johnson@madetech.com>
2025-01-03 15:17:33 +0000

    Added new letter personalisations. Unit tests passing.

--- backoffice/src/Models/ViewModels/LetterPersonalisationView.cs
+++ backoffice/src/Models/ViewModels/LetterPersonalisationView.cs
@@ -1,6 +1,7 @@
 #region
 
 using System.Text.RegularExpressions;
+using Droits.Helpers;
 using Droits.Models.Entities;
 
 #endregion
@@ -11,16 +12,31 @@ public class LetterPersonalisationView
 {
     public LetterPersonalisationView(Droit droit)
     {
+        Pluralised = droit.WreckMaterials.Count > 1;
         Reference = droit.Reference;
         Date = droit.ReportedDate.ToString("dd/MM/yyyy");
-        Wreck = droit?.Wreck?.Name ?? "---";
-        
+        Wreck = droit.Wreck?.Name ?? "";
+        ItemPluralised = Pluralised ? "items" : "item";
+        HasPluralised = Pluralised ? "have" : "has";
+        ThisPluralised  = Pluralised ? "these" : "this";
+        IsPluralised = Pluralised ? "are" : "is";
+        PiecePluralised = Pluralised ? "pieces" : "piece";
+        Items = String.Join('\n', droit.WreckMaterials
+            .Select(material => String.Concat("* ", material.Name)).ToList()) ?? "";
+        FullName = droit.Salvor?.Name ?? "";
     }
 
-
+    private bool Pluralised { get; }
     private string Reference { get; }
     private string Date { get; }
     private string Wreck { get; }
+    private string ItemPluralised { get; }
+    private string HasPluralised  { get; }
+    private string ThisPluralised { get; }
+    private string IsPluralised { get; }
+    private string PiecePluralised { get; }
+    private string Items { get; }
+    private string FullName { get; }
 
     public Dictionary<string, dynamic> GetAsPersonalisation()
     {
@@ -28,7 +44,14 @@ public class LetterPersonalisationView
         {
             { "reference", Reference },
             { "wreck", Wreck },
-            { "date", Date }
+            { "date", Date },
+            { "item pluralised", ItemPluralised },
+            { "has pluralised", HasPluralised },
+            { "this pluralised", ThisPluralised },
+            { "is pluralised", IsPluralised },
+            { "piece pluralised", PiecePluralised },
+            { "items", Items },
+            { "full name", FullName }
         };
     }
 
@@ -37,29 +60,15 @@ public class LetterPersonalisationView
     {
         foreach ( var param in GetAsPersonalisation() )
         {
+            // Capitalise pluralised word if it's at the start of a sentence.
+            if ( param.Key.Contains("pluralised") )
+            {
+                content = Regex.Replace(content, $@"(?<=[\.\:\?\!]\s|[\.\:\?\!]\)\s|^)\(\({param.Key}\)\)(?=.*[\.\:\?\!])", StringHelper.CapitalizeFirstLetter(param.Value.ToString()), RegexOptions.Multiline);
+            }
+
             content = Regex.Replace(content, $@"\(\({param.Key}\)\)", param.Value.ToString(), RegexOptions.IgnoreCase);
         }
 
         return content;
     }
 }
-
-//         Known:
-//         "reference"
-//             
-//         "wreck" //wreck name
-//         "date" // reported date
-//             
-//             
-//         Unknown:
-//         "custom message" // No longer needed as can edit on the fly. 
-//         "item pluralised" (item/items? depending on wreck material count?)
-//         "has pluralised"? (is/has depending on wreck material count?)
-//         "this pluralised" (this/these depending on wreck material count?)
-//         "is pluralised" (is/are ?? depending on wreck material count?)
-//         "items" //? 
-//         "suggested donate" // bool
-//         "wreck known" //bool ?
-//         "link_to_file" // copy of report submission? 
-//         "late report"//bool ? 
-//             
