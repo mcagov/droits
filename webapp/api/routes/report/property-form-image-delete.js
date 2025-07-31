483b737534732cdfafbf87b14a6070c50418ddc7
Denise T <81650088+Dtzz81@users.noreply.github.com>
2025-07-31 10:32:34 +0100

    Update property-form-image-delete.js
    should ok now, deleted lines back

--- webapp/api/routes/report/property-form-image-delete.js
+++ webapp/api/routes/report/property-form-image-delete.js
@@ -12,6 +12,11 @@ export default function (app) {
     const id = req.params.prop_id;
     const image = req.session.data.property[id].image;
     fs.unlink(`uploads/${image}`, (err) => {
+        if (err) console.log(err);
+      else {
+        console.log(`\nDeleted file @ uploads/${image}`);
+      }
+    });
     req.session.data.property[id].image = '';
     req.session.save();
     res.json();
