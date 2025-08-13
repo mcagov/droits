de9bea2f754ad51abe001d55c4ceff41c0c20cb0
denise.turkova <denise.turkova@madetech.com>
2025-08-13 10:12:03 +0100

    added test for users to view a page

--- webapp/utilities/matchRoutes.test.js
+++ webapp/utilities/matchRoutes.test.js
@@ -1,15 +1,10 @@
 import request from "supertest";
 import app from "../server";
 
-
 describe('User can visit the page', () => {
     it('should display the correct page with HTML content', async () => {
-        try {
             const response = await request(app).get('/portal/start');
-
             expect(response.statusCode).toBe(200);
-        }    finally
-        
             
     });
 });
