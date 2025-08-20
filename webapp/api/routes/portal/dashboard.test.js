0027fd7395f735b00544286603ff655092402e88
denise.turkova <denise.turkova@madetech.com>
2025-08-20 11:05:14 +0100

    removed unnecessary tests

--- webapp/api/routes/portal/dashboard.test.js
+++ webapp/api/routes/portal/dashboard.test.js
@@ -1,31 +1,11 @@
 import "../../../jest.config.js"
+import request from 'supertest';
+import nock from 'nock';
 
 require('dotenv-json')();
 
-const request = require('supertest');
-const app = require('../../../server.js');
-const nock = require('nock'); //use nock to mock HTTP calls
-
-
 beforeEach(() => {
     nock.cleanAll(); // reset all mocks
-});
-describe('Request received', () => {
-    it('should respond with a status code other than 400', async () => {
-        const res = request('http://localhost:3000').get('/portal/dashboard');
-
-        expect(res.statusCode).not.toBe(400);
-    });
-    it('should respond with a 200 OK status', async () => {
-        nock('http://localhost:3000') // set up a mock server
-            .get('/portal/dashboard') // <- intercept this GET request
-            .reply(200, { success: true }); // <- fake a 200 OK response with a body
-
-        const res = await request('http://localhost:3000').get('/portal/dashboard');
-
-        expect(res.statusCode).toBe(200);
-    });
-
 });
 
 describe('Rate limit exceeded', () => {
