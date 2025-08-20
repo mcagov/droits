b4e11717209b3bdfda6bf3bd3768ad18b804cba0
Ahmad Vegah <ahmadvegah@gmail.com>
2025-08-20 13:20:12 +0100

    fix: remove redundant code
    This code does not work and is not needed as the path is not a user provided value. Other paths return 404.

--- webapp/utilities/matchRoutes.test.js
+++ webapp/utilities/matchRoutes.test.js
@@ -3,9 +3,8 @@ import app from "../server";
 
 describe('User can visit the page', () => {
     it('should display the correct page with HTML content', async () => {
-            const response = await request(app).get('/portal/start');
-            expect(response.statusCode).toBe(200);
-            
+        const response = await request(app).get('/portal/start');
+        expect(response.statusCode).toBe(200);
     });
 });
             
