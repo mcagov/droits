b8d2f909f60eba4a6f840ebd160cb3da5734ec4b
denise.turkova <denise.turkova@madetech.com>
2025-08-27 11:12:06 +0100

    refactoring and making tests pass

--- webapp/utilities/droitsRateLimiter.js
+++ webapp/utilities/droitsRateLimiter.js
@@ -1,15 +1,23 @@
-const { RateLimiterMemory } = require("rate-limiter-flexible");
+import { RateLimiterMemory } from 'rate-limiter-flexible';
+import config from "../app/config";
+
+// const { RateLimiterMemory } = require("rate-limiter-flexible");
 
 const POINTS = config.RATE_LIMIT_POINTS ? config.RATE_LIMIT_POINTS : 10;
 const DURATION = config.RATE_LIMIT_DURATION ? config.RATE_LIMIT_DURATION : 60;
 
+const rateLimiter = new RateLimiterMemory({
+    points: POINTS,
+    duration: DURATION,
+});
+
 // const MAX_REQUESTS = 10;
 // const WINDOW_MS = 60 * 1000;
 
-const rateLimiter = new RateLimiterMemory({
-    points : MAX_REQUESTS,
-    duration : WINDOW_MS / 1000,
-});
+// const rateLimiter = new RateLimiterMemory({
+//     points : MAX_REQUESTS,
+//     duration : WINDOW_MS / 1000,
+// });
 
 /**
  * Express middleware to handle rate limiting.
