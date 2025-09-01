9adf66faf947cc584762eb9c07e13d04f240ba2b
denise.turkova <denise.turkova@madetech.com>
2025-09-01 13:25:13 +0100

    chore: added test for h1

--- tests/cypress/e2e/spec.cy.js
+++ tests/cypress/e2e/spec.cy.js
@@ -3,3 +3,10 @@ describe('template spec', () => {
     cy.visit('/')
   })
 })
+
+describe('Homepage', () => {
+  it('should display the main heading', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Report Wreck Material').should('be.visible')
+  })
+})
