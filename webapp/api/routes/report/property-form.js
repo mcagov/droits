31447dde715524d6fb3455734783e0b42697af33
Will Fort <will.fort@madetech.com>
2026-04-09 13:31:44 +0100

    added minimum of 1 for the quantity

--- webapp/api/routes/report/property-form.js
+++ webapp/api/routes/report/property-form.js
@@ -72,7 +72,7 @@ export default function (app) {
       await body('property' + '[' + propertyID + '][quantity]')
         .exists()
         .escape()
-        .isNumeric()
+        .isInt({ min:1 })
         .withMessage('Enter a number')
         .not()
         .isEmpty()
